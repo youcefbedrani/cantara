81b018711e4a010c9dbaa023490753f28b690aff
youcefbedrani <youcefbedrani99@gmail.com>
2026-06-05 16:06:01 +0100

    Remove public/index.html, simplify server to serve cantara-landing.html only

--- server.js
+++ server.js
@@ -1,29 +1,12 @@
-require('dotenv').config();
 const express = require('express');
 const path = require('path');
-const fs = require('fs');
-
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-const types = {
-  '.png': 'image/png', '.jpg': 'image/jpeg', '.jpeg': 'image/jpeg',
-  '.svg': 'image/svg+xml', '.ico': 'image/x-icon', '.html': 'text/html; charset=utf-8',
-  '.css': 'text/css', '.js': 'text/javascript',
-};
-
-app.get(/\.(png|jpg|jpeg|svg|ico|css|js)$/, (req, res) => {
-  const file = path.join(__dirname, req.path);
-  if (fs.existsSync(file)) {
-    res.type(types[path.extname(file)] || 'application/octet-stream');
-    res.sendFile(file);
-  } else {
-    res.status(404).end();
-  }
-});
+app.use(express.static(__dirname));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'cantara-landing.html'));
 });
 
-app.listen(PORT, () => console.log('Running'));
+app.listen(PORT, () => console.log('Running on port ' + PORT));
