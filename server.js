a6051d8935386553a0da2980f3a16e9928db5458
youcefbedrani <youcefbedrani99@gmail.com>
2026-06-05 17:37:17 +0100

    Add angles section, update photos, create A/B test v2 with pre-cut sizes

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.static(__dirname));
 
+app.get('/v2', (req, res) => {
+  res.sendFile(path.join(__dirname, 'cantara-landing-v2.html'));
+});
+
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'cantara-landing.html'));
 });
